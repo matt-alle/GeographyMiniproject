28dd5602a1377e4ed4c3197a511617d068fc70ea
ma <ma@Matthias>
2020-10-21 17:54:05 +0200

    added HelloJavaFX for testing

--- src/geographyMiniproject/Lalal.java
+++ src/geographyMiniproject/Lalal.java
@@ -1,10 +1,24 @@
 package geographyMiniproject;
 
-public class Lalal {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class Lalal extends Application {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch();
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Label lblHello = new Label("Hello, JavaFX!");
+		BorderPane root = new BorderPane();
+		root.setCenter(lblHello);
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Hello, JavaFX!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
